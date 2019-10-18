bbfd51afbea33af29c8584281325cc1400b48fab
Vivek Yadav <vivekyadav1207731111@gmail.com>
2019-10-18 11:11:47 +0530

    more memory efficient Structure Now.

--- structure/structA.h
+++ structure/structA.h
@@ -1,8 +1,20 @@
 struct A
 {
     char symbol;			// 1 byte in memory
+    short id;				// 2 bytes in memory
+    int number;				// 4 bytes - no padding needed
+};//Total 8 Bytes
+
+/*
+This Structure is taking total 12 Bytes in memory.
+
+struct A
+{
+    char symbol;			// 1 byte in memory
     //char charPadding[3];    // 3 bytes padding     
     int number;				// 4 bytes - no padding needed
     short id;				// 2 bytes in memory
 	//char shortPadding[2];   // 2 bytes padding  
 };
+
+*/
